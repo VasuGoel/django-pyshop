5151b8d2c9aca79426189a1049e5bc3ae82c6d07
Vasu Goel <goelvasu0712@gmail.com>
2019-06-08 16:08:01 +0530

    Create ProductAdmin for list_display()

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Product
 
 # Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+admin.site.register(Product, ProductAdmin)
