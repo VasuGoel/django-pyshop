acffa5364a517f1edcce7de07c83b290e79f9b33
Vasu Goel <goelvasu0712@gmail.com>
2019-06-10 00:40:11 +0530

    Render index templates instead of HttpResponse()

--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome to the products page!')
+    products = Product.objects.all()
+    # return HttpResponse('Welcome to the products page!')
+    return render(request, 'index.html')
